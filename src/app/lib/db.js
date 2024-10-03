2ee66f9bdc2ad4fa4616ae98ffdd38f8531f258d
bhanusunrise <pasindubhanukagood@gmail.com>
2024-10-03 16:06:39 +0530

    first commit

---
+++ src/app/lib/db.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+global.mongoose = {
+    conn: null,
+    promise: null,
+};
+
+export async function dbConnect(){
+    if(global.mongoose && global.mongoose.conn){
+        console.log("Connected from previouse");
+        return global.mongoose.conn;
+    } else{
+        const conString = process.env.CONNECTION_STRING;
+    
+        const promise = mongoose.connect(conString, {
+            autoIndex: true,
+        });
+
+        global.mongoose = {
+            conn: await promise,
+            promise,
+        };
+
+        console.log("Newly Connected");
+
+        return await promise;
+    }
+
+}
