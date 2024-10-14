7284516faaae7d0bcd95478153f3a4828c05f541
watagalawgbn <watagalawgbn.21@uom.lk>
2024-10-14 15:33:18 -0700

    simple UI created

---
+++ src/app/api/prospects/route.jsx
@@ -0,0 +1,25 @@
+// src/app/api/prospects/route.jsx
+
+
+import { dbConnect } from "@/app/lib/db";
+import Prospect from "@/app/models/Prospect";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();  
+
+    const prospectsFromDB = await Prospect.find(); // Retrieve all prospects from the database
+    console.log("Fetched prospects:", prospectsFromDB);
+
+    // Return prospects or empty array if none found
+    if (prospectsFromDB.length === 0) {
+      return NextResponse.json({ Prospects: [] });
+    } else {
+      return NextResponse.json({ Prospects: prospectsFromDB }); // Return fetched prospects
+    }
+  } catch (error) {
+    console.error("Error fetching prospects:", error);
+    return NextResponse.json({ error: "Error fetching prospects" }, { status: 500 });
+  }
+}
