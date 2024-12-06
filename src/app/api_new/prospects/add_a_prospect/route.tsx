582f9cb1e8f5c0e0f45a007cd66c1f1b0cdc4b79
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-12-06 16:02:45 +0530

    Prospect Request done without bhanu Integration

--- src/app/api_new/prospects/add_a_prospect/route.tsx
+++ src/app/api_new/prospects/add_a_prospect/route.tsx
@@ -4,25 +4,31 @@ import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
 interface IProspectRequest {
-  company_id: String;
-  companyName: String;
-  companyAddress: String;
-  contactPersonName: String;
-  contactPersonNumber: String;
-  contactPersonEmail: String;
-  producttype: String;
-  comment: String;
-  industry_id: String;
+  companyId: string;
+  companyName: string;
+  companyAddress: string;
+  contactPersonName: string;
+  contactPersonNumber: string;
+  contactPersonEmail: string;
+  productId: string;
+  comment: string;
+  industryId: string;
 }
 
 
 export async function POST(req: Request) {
   try {
+    var company = null;
     const prospect: IProspectRequest = await req.json();
     const client = await clientPromise;
     const db = client.db(process.env.DB_NAME);
 
     var createCompany = false;
+
+    //Map all other Product ID to the SameProduct ID
+    if(prospect.productId != "6734053c308fd8d176381e07"){
+      prospect.productId = "6734054e308fd8d176381e08";
+    }
 
     // Set current date as the date added
     const dateAdded = new Date();
@@ -34,42 +40,77 @@ export async function POST(req: Request) {
     dateExpires.setMonth(dateExpires.getMonth() + 3);
 
     //Check whether the company already exists
-    if (prospect.company_id == "" || prospect.company_id == null) {
+    if (prospect.companyId == "" || prospect.companyId == null) {
       createCompany = true;
     } else {
-      const company = await db.collection("Companies").findOne({ _id: prospect.company_id });
+      console.log("Test API Company ID " + prospect.companyId);
+
+      company = await db.collection("Companies").findOne({ _id: new ObjectId(prospect.companyId.toString()) });
+
+      console.log("Test1 API Company " + company);
       if (!company) {
         createCompany = true;
       }
     }
 
     if (createCompany) {
-      const result = await db.collection("Companies").insertOne({
+      const createCompanyResult = await db.collection("Companies").insertOne({
         companyName: prospect.companyName,
         companyAddress: prospect.companyAddress,
         contactPersonName: prospect.contactPersonName,
         contactPersonNumber: prospect.contactPersonNumber,
         contactPersonEmail: prospect.contactPersonEmail,
-        industry_id: prospect.industry_id,
+        industry_id: prospect.industryId,
         comment: prospect.comment,
         approved: false
       });
-      prospect.company_id = result.insertedId.toString();
+      prospect.companyId = createCompanyResult.insertedId.toString();
+
+
+      company = await db.collection("Companies").findOne({ _id: new ObjectId(prospect.companyId.toString()) });
     }
 
-    const result = await db.collection("Prospects").insertOne({
-      company_id: prospect.company_id,
-      product_type_id: prospect.producttype,
-      entity_id: entity_id,
-      date_added: dateAdded,
-      date_expires: dateExpires,
-      contactPersonName: prospect.contactPersonName,
-      contactPersonNumber: prospect.contactPersonNumber,
-      contactPersonEmail: prospect.contactPersonEmail,
-      status: PROSPECT_VALUES[0].value
-    });
+    //Get Current Company partnership status both product and event
+    //Get Prospect partnership type
+    if (company != null) {
 
-    return NextResponse.json({ success: true });
+      const lastRecord = await db.collection("Prospects")
+        .find({ company_id: prospect.companyId.toString(), product_type_id: prospect.productId.toString() })
+        .sort({ _id: -1 })
+        .limit(1)
+        .toArray();
+
+      if (lastRecord.length > 0) {
+        const prospectExpiresDate = new Date(lastRecord[0].date_expires);
+        const currentDate = new Date();
+
+        if (prospectExpiresDate > currentDate) {
+          console.log("The expiration date is in the future.");
+          //Integrate with bhanukas API for Queue
+        }
+
+      }
+       
+      const prospectResult = await db.collection("Prospects").insertOne({
+        company_id: prospect.companyId,
+        product_type_id: prospect.productId,
+        entity_id: entity_id,
+        date_added: dateAdded,
+        date_expires: dateExpires,
+        contactPersonName: prospect.contactPersonName,
+        contactPersonNumber: prospect.contactPersonNumber,
+        contactPersonEmail: prospect.contactPersonEmail,
+        status: PROSPECT_VALUES[0].value
+      });
+
+      return NextResponse.json({ success: true });
+    }
+
+
+    //return NextResponse.json({ success: true });
+
+    return NextResponse.json({ error: "Failed to update request" },
+      { status: 500 });
 
   } catch (e) {
     console.error("Error updating request:", e);
