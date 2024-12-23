5a3e5dc5b8c8f2962be7de2155c73c9ce8f0ea45
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-12-23 17:35:23 +0530

    UI Changes

--- src/app/dashboard/prospect/submit_request/page.tsx
+++ src/app/dashboard/prospect/submit_request/page.tsx
@@ -4,6 +4,14 @@ import React, { useEffect, useState } from 'react';
 import { IRequest } from '@/app/models/RequestTypes';
 import { fetchCompany, fetchProducts, fetchIndustry, submitProspect, FormData, Industry, Product, fetchCompanyQuery, ICompanyQuery } from './functions';
 import { format } from 'date-fns';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/components/ui/table";
 
 const Page: React.FC = () => {
 
@@ -121,24 +129,25 @@ const Page: React.FC = () => {
 
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg">
-        <h2 className="text-xl font-semibold mb-6">Add New Prospect Request</h2>
+    <div className="grid grid-cols-3 h-screen">
+      <div className="col-span-2 p-6">
+        <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg">
+          <h2 className="text-xl font-semibold mb-6">Add New Prospect Request</h2>
 
-        <div className="mb-4 relative">
-          <label htmlFor="name" className="block text-sm font-medium mb-1">
-            Company Name
-          </label>
-          <input
-            id="companyName"
-            type="text"
-            name="companyName"
-            onChange={handleChange}
-            value={formData.companyName as string}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-          {/*
+          <div className="mb-4 relative">
+            <label htmlFor="name" className="block text-sm font-medium mb-1">
+              Company Name
+            </label>
+            <input
+              id="companyName"
+              type="text"
+              name="companyName"
+              onChange={handleChange}
+              value={formData.companyName as string}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+            {/*
           {showDropdown && (
             <ul className="absolute bg-white border border-gray-300 rounded-md mt-1 w-full max-h-40 overflow-y-auto">
               {searchResults.map((result) => (
@@ -153,172 +162,201 @@ const Page: React.FC = () => {
             </ul>
           )}*/}
 
-          {showDropdown && (
-            <ul className="absolute bg-white border border-gray-300 rounded-md mt-1 w-full max-h-40 overflow-y-auto">
-              {searchResults.map((result) => (
-                <li
-                  key={result._id}
-                  onClick={() => handleSelectCompany(result._id)}
-                  className="p-2 cursor-pointer hover:bg-blue-500 hover:text-white"
-                >
-                  <div>
-                    <span className="font-semibold">{result.companyName}</span>
-                  </div>
-                  <div className="ml-4 mt-1 text-sm text-gray-500">
+            {showDropdown && (
+              <ul className="absolute bg-white border border-gray-300 rounded-md mt-1 w-full max-h-40 overflow-y-auto">
+                {searchResults.map((result) => (
+                  <li
+                    key={result._id}
+                    onClick={() => handleSelectCompany(result._id)}
+                    className="p-2 cursor-pointer hover:bg-blue-500 hover:text-white"
+                  >
+                    <div>
+                      <span className="font-semibold">{result.companyName}</span>
+                    </div>
+                    <div className="ml-4 mt-1 text-sm text-gray-500">
 
-                    {result.dateexpiresEvent && (
-                      <div>Product Partnership Expires: {format(result.dateexpiresEvent,"MMMM dd, yyyy hh:mm a")}</div>
-                    )}
+                      {result.dateexpiresEvent && (
+                        <div>Product Partnership Expires: {format(result.dateexpiresEvent, "MMMM dd, yyyy hh:mm a")}</div>
+                      )}
 
-                    {result.dateexpiresProduct && (
-                      <div>EventPartnership Expires: {format(result.dateexpiresProduct,"MMMM dd, yyyy hh:mm a")}</div>
-                    )}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
+                      {result.dateexpiresProduct && (
+                        <div>EventPartnership Expires: {format(result.dateexpiresProduct, "MMMM dd, yyyy hh:mm a")}</div>
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
 
-          {/* {suggestedPartnership && (
+            {/* {suggestedPartnership && (
           <p className="text-sm text-gray-600 mt-1">
             Suggested Partnership: Try a <strong>{suggestedPartnership}</strong> partnership.
           </p>
         )} */}
 
-        </div>
-        <div className="mb-4">
-          <label htmlFor="companyAddress" className="block text-sm font-medium mb-1">
-            Company Address
-          </label>
-          <textarea
-            autoComplete="off"
-            id="companyAddress"
-            name="companyAddress"
-            value={formData.companyAddress as string}
-            onChange={handleChange}
-            rows={3}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="companyAddress" className="block text-sm font-medium mb-1">
+              Company Address
+            </label>
+            <textarea
+              autoComplete="off"
+              id="companyAddress"
+              name="companyAddress"
+              value={formData.companyAddress as string}
+              onChange={handleChange}
+              rows={3}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="contactPersonName" className="block text-sm font-medium mb-1">
-            Contact Person Name
-          </label>
-          <input
-            id="contactPersonName"
-            type="text"
-            name="contactPersonName"
-            value={formData.contactPersonName as string}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+          <div className="mb-4">
+            <label htmlFor="contactPersonName" className="block text-sm font-medium mb-1">
+              Contact Person Name
+            </label>
+            <input
+              id="contactPersonName"
+              type="text"
+              name="contactPersonName"
+              value={formData.contactPersonName as string}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="contactPersonNumber" className="block text-sm font-medium mb-1">
-            Contact Person Contact Number
-          </label>
-          <input
-            id="contactPersonNumber"
-            type="text"
-            name="contactPersonNumber"
-            value={formData.contactPersonNumber as string}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+          <div className="mb-4">
+            <label htmlFor="contactPersonNumber" className="block text-sm font-medium mb-1">
+              Contact Person Contact Number
+            </label>
+            <input
+              id="contactPersonNumber"
+              type="text"
+              name="contactPersonNumber"
+              value={formData.contactPersonNumber as string}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="contactPersonEmail" className="block text-sm font-medium mb-1">
-            Contact Person Email Address
-          </label>
-          <input
-            id="contactPersonEmail"
-            type="email"
-            name="contactPersonEmail"
-            value={formData.contactPersonEmail as string}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+          <div className="mb-4">
+            <label htmlFor="contactPersonEmail" className="block text-sm font-medium mb-1">
+              Contact Person Email Address
+            </label>
+            <input
+              id="contactPersonEmail"
+              type="email"
+              name="contactPersonEmail"
+              value={formData.contactPersonEmail as string}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="producttype" className="block text-sm font-medium mb-1">
-            Select a Industry
-          </label>
+          <div className="mb-4">
+            <label htmlFor="producttype" className="block text-sm font-medium mb-1">
+              Select a Industry
+            </label>
 
-          <select
-            id="industry"
-            name="industry_id"
-            value={formData.industry_id as string} // Bind the dropdown to formData.industry
-            onChange={handleChange}   // Update formData when a new industry is selected
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          >
-            <option value="" disabled>
-              -- Select an Industry --
-            </option>
-            {industries.map((industry) => (
-              <option key={industry._id} value={industry._id} >
-                {industry.industryName}
+            <select
+              id="industry"
+              name="industry_id"
+              value={formData.industry_id as string} // Bind the dropdown to formData.industry
+              onChange={handleChange}   // Update formData when a new industry is selected
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            >
+              <option value="" disabled>
+                -- Select an Industry --
               </option>
-            ))}
-          </select>
-        </div>
+              {industries.map((industry) => (
+                <option key={industry._id} value={industry._id} >
+                  {industry.industryName}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="producttype" className="block text-sm font-medium mb-1">
-            Select a Product Type
-          </label>
+          <div className="mb-4">
+            <label htmlFor="producttype" className="block text-sm font-medium mb-1">
+              Select a Product Type
+            </label>
 
-          <select
-            id="producttype"
-            name="productId"
-            value={formData.productId as string} // Bind the dropdown to formData.industry
-            onChange={handleChange}   // Update formData when a new industry is selected
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          >
-            <option value="" disabled>
-              -- Select an Product Type --
-            </option>
-            {products.map((product) => (
-              <option key={product._id} value={product._id}>
-                {product.productName}
+            <select
+              id="producttype"
+              name="productId"
+              value={formData.productId as string} // Bind the dropdown to formData.industry
+              onChange={handleChange}   // Update formData when a new industry is selected
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            >
+              <option value="" disabled>
+                -- Select an Product Type --
               </option>
-            ))}
-          </select>
+              {products.map((product) => (
+                <option key={product._id} value={product._id}>
+                  {product.productName}
+                </option>
+              ))}
+            </select>
 
 
 
-        </div>
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="comment" className="block text-sm font-medium mb-1">
-            Comments
-          </label>
-          <textarea
-            id="comment"
-            name="comment"
-            value={formData.comment as string}
-            onChange={handleChange}
-            rows={3}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
+          <div className="mb-4">
+            <label htmlFor="comment" className="block text-sm font-medium mb-1">
+              Comments
+            </label>
+            <textarea
+              id="comment"
+              name="comment"
+              value={formData.comment as string}
+              onChange={handleChange}
+              rows={3}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
 
-        <button
-          type="submit"
-          className="w-full py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          Submit Request
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="w-full py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Submit Request
+          </button>
+        </form>
+      </div>
+
+      <div className="p-3">
+      <Table>
+        <TableHeader>
+          <TableRow>
+
+            <TableHead>Company Name</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Submission Date</TableHead>
+            <TableHead>Product Type</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+
+            <TableRow key="5">
+              <TableCell>Ashan</TableCell>
+              <TableCell>
+              Ashan
+              </TableCell>
+              <TableCell>Ashan</TableCell>
+              <TableCell>Ashan</TableCell>
+              <TableCell>Ashan</TableCell>
+            </TableRow>
+
+        </TableBody>
+      </Table>
+      </div>
+
 
     </div>
   );
