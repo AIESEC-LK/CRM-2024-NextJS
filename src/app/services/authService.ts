c7a2cb2db0f157f3189ac9780a7b085529036b6b
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-12-25 15:32:39 +0530

    Major changes done

--- src/app/services/authService.ts
+++ src/app/services/authService.ts
@@ -1,32 +1,27 @@
 class AuthService {
     private static TOKEN_KEY = 'jwt_token';
-    private static USER_ID = 'user_details';
-    private static USER_LC_ID = 'user_details';
+    private static USER_KEY = 'user_details';
   
     static saveToken(token: string): void {
       localStorage.setItem(AuthService.TOKEN_KEY, token);
     }
   
-    static saveUserId(user: object): void {
-      localStorage.setItem(AuthService.USER_ID, JSON.stringify(user));
+    static saveUserDetails(user: object): void {
+      localStorage.setItem(AuthService.USER_KEY, JSON.stringify(user));
     }
-
-    static saveUserLCId(user: object): void {
-        localStorage.setItem(AuthService.USER_LC_ID, JSON.stringify(user));
-      }
   
     static getToken(): string | null {
       return localStorage.getItem(AuthService.TOKEN_KEY);
     }
   
     static getUserDetails(): object | null {
-      const userDetails = localStorage.getItem(AuthService.USER_ID);
+      const userDetails = localStorage.getItem(AuthService.USER_KEY);
       return userDetails ? JSON.parse(userDetails) : null;
     }
   
     static clear(): void {
       localStorage.removeItem(AuthService.TOKEN_KEY);
-      localStorage.removeItem(AuthService.USER_ID);
+      localStorage.removeItem(AuthService.USER_KEY);
     }
   }
   
