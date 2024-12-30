68467c2ef5b4793b3e5090232761814f8202b4c4
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-12-31 04:35:25 +0530

    USER_ROLE_VALUES and User Endpoint created

---
+++ src/app/api_new/user/add_a_user/route.tsx
@@ -0,0 +1,140 @@
+import clientPromise from "@/app/lib/mongodb";
+import { PROSPECT_VALUES } from "@/app/lib/values";
+import { NextResponse } from "next/server";
+import { ObjectId } from "mongodb";
+
+interface IProspectRequest {
+    companyId: string;
+    companyName: string;
+    companyAddress: string;
+    contactPersonName: string;
+    contactPersonNumber: string;
+    contactPersonEmail: string;
+    productId: string;
+    comment: string;
+    industryId: string;
+}
+
+interface IUserCreateRequest {
+    userEmail: string;
+    userRole: string;
+    userEntityId: string;
+}
+
+export async function POST(req: Request) {
+    try {
+        let company = null;
+        let newCompany = false;
+
+        const userCreateRequest: IUserCreateRequest = await req.json();
+        const client = await clientPromise;
+        const db = client.db(process.env.DB_NAME);
+
+        let createCompany = false;
+
+        // Set current date as the date added
+        const dateAdded = new Date();
+
+        /* TODO*/
+        //Fetch from Auth
+        const entity_id = userCreateRequest.userEntityId;
+
+        //Check whether the user already exists
+        if (userCreateRequest.userEmail == "" || userCreateRequest.userRole == null || userCreateRequest.userEntityId == null) {
+            return NextResponse.json({ error: "Invalid Request Body Payload" },
+                { status: 400 });
+        } else {
+            company = await db.collection("Companies").findOne({ _id: new ObjectId(prospect.companyId.toString()) });
+            if (!company) {
+                createCompany = true;
+            }
+        }
+
+        if (createCompany) {
+            const createCompanyResult = await db.collection("Companies").insertOne({
+                companyName: prospect.companyName,
+                companyAddress: prospect.companyAddress,
+                contactPersonName: prospect.contactPersonName,
+                contactPersonNumber: prospect.contactPersonNumber,
+                contactPersonEmail: prospect.contactPersonEmail,
+                industry_id: prospect.industryId,
+                comment: prospect.comment,
+                approved: false
+            });
+            /*TODO
+            //Integrate with Raveens`s API for Company Approvel currently prospect will places
+            **
+            **
+            **
+            */
+            prospect.companyId = createCompanyResult.insertedId.toString();
+            newCompany = true;
+            company = await db.collection("Companies").findOne({ _id: new ObjectId(prospect.companyId.toString()) });
+        }
+
+        //Get Current Company partnership status both product and event
+        //Get Prospect partnership type
+        if (company != null) {
+
+            if (!newCompany) {
+                const lastRecord = await db.collection("Prospects")
+                    .find({ company_id: prospect.companyId.toString(), product_type_id: prospect.productId.toString() })
+                    .sort({ _id: -1 })
+                    .limit(1)
+                    .toArray();
+
+                if (lastRecord.length > 0) {
+                    const prospectExpiresDate = new Date(lastRecord[0].date_expires);
+                    const currentDate = new Date();
+
+                    if (prospectExpiresDate > currentDate) {
+                        console.log("The expiration date is in the future.");
+
+                        await db.collection("Pending_Prospects").insertOne({
+                            company_id: prospect.companyId,
+                            product_type_id: prospect.productId,
+                            entity_id: entity_id,
+                            date_added: dateAdded,
+                            contactPersonName: prospect.contactPersonName,
+                            contactPersonNumber: prospect.contactPersonNumber,
+                            contactPersonEmail: prospect.contactPersonEmail,
+                            status: PROSPECT_VALUES[1].value,
+                        });
+
+                        return NextResponse.json({ error: "The expires on " + prospectExpiresDate + ". Prospect Request Added to Queue " },
+                            { status: 200 });
+                    }
+
+                }
+
+            }
+
+            //Create Prospect
+            await db.collection("Prospects").insertOne({
+                company_id: prospect.companyId,
+                product_type_id: prospect.productId,
+                entity_id: entity_id,
+                date_added: dateAdded,
+                date_expires: dateExpires,
+                contactPersonName: prospect.contactPersonName,
+                contactPersonNumber: prospect.contactPersonNumber,
+                contactPersonEmail: prospect.contactPersonEmail,
+                status: PROSPECT_VALUES[1].value,
+                newCompay: newCompany
+            });
+
+            return NextResponse.json({ success: true });
+        }
+
+
+        return NextResponse.json({ error: "Failed to update request" },
+            { status: 500 });
+
+    } catch (e) {
+        console.error("Error updating request:", e);
+        return NextResponse.json(
+            { error: "Failed to update request" },
+            { status: 500 }
+        );
+    }
+}
