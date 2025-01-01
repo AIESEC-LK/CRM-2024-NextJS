4c2867304bc6e7d0d35f3df2243a0d9c88dbd7ec
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2025-01-01 20:58:52 +0530

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,54 +18,59 @@ const LoginPage = () => {
     router.push(PARTNERHSIPS_UI_PATH);
 
     //Mock user login
-    const mockUser = { email: 'ashanmatheesha@gmail.com', role: "member" as "member", lcId: '675dbabf296393f677c5cf21' }
+    const mockUser = {
+      email: 'ashanmatheesha@gmail.com',
+      role: "member",
+      lcId: '675dbabf296393f677c5cf21',
+    } as const; // Use `as const` here
     login(mockUser);
+
   };
 
   return (
-      <div
-        className="relative flex items-center justify-center min-h-screen bg-cover bg-center"
-      >
-        <div className="absolute inset-0 bg-gray-900"></div>
-        <div className="relative w-full max-w-sm p-6 bg-white rounded-md shadow-2xl">
-          <h1 className="text-2xl font-semibold text-center text-gray-800">Login</h1>
-          <form className="mt-4 space-y-4" onSubmit={handleSubmit}>
-            <div>
-              <Label htmlFor="email">Email</Label>
-              <Input
-                type="email"
-                id="email"
-                placeholder="Enter your email"
-                className="mt-1"
-              />
-            </div>
-            <div>
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="Enter your password"
-                className="mt-1"
-              />
-            </div>
-            <div className="space-y-4">
-              <Button
-                type="submit"
-                className="w-full bg-[#037ef3] text-white hover:bg-[#0367c4]"
-              >
-                Login
-              </Button>
-              <Button
-                type="reset"
-                variant="outline"
-                className="w-full bg-[#caccd1] text-gray-700 hover:bg-[#b8babc]"
-              >
-                Reset
-              </Button>
-            </div>
-          </form>
-        </div>
+    <div
+      className="relative flex items-center justify-center min-h-screen bg-cover bg-center"
+    >
+      <div className="absolute inset-0 bg-gray-900"></div>
+      <div className="relative w-full max-w-sm p-6 bg-white rounded-md shadow-2xl">
+        <h1 className="text-2xl font-semibold text-center text-gray-800">Login</h1>
+        <form className="mt-4 space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              className="mt-1"
+            />
+          </div>
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              className="mt-1"
+            />
+          </div>
+          <div className="space-y-4">
+            <Button
+              type="submit"
+              className="w-full bg-[#037ef3] text-white hover:bg-[#0367c4]"
+            >
+              Login
+            </Button>
+            <Button
+              type="reset"
+              variant="outline"
+              className="w-full bg-[#caccd1] text-gray-700 hover:bg-[#b8babc]"
+            >
+              Reset
+            </Button>
+          </div>
+        </form>
       </div>
+    </div>
   );
 };
 
