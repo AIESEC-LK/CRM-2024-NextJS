e03d607a480c2caca788d6293472de2484ab42fa
Yasanjith Rajapathirane <125336683+YasanjithR@users.noreply.github.com>
2025-01-06 12:07:57 +0530

    prospects expire date changed to 15

--- src/app/lib/values.tsx
+++ src/app/lib/values.tsx
@@ -24,7 +24,7 @@ export const USER_ROLE_VALUES = [
 
 export const AUTONOMATION_TABLE_ID = "6769349892a256157cb6142c"
 
-export const PROSPECT_EXPIRE_TIME_DURATION = 1000 * 60 * 60 * 24 * 14
+export const PROSPECT_EXPIRE_TIME_DURATION = 1000 * 60 * 60 * 24 * 15
 export const LEAD_EXPIRE_TIME_DURATION = 1000 * 60 * 60 * 24 * 30
 
 //export const BASE_URL = "http://localhost:3001";
