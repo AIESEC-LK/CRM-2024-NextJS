14eb2e2ffc4fc77cee52977b6d1910a9e0120f62
chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2025-05-19 21:44:50 +0530

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
 import { Label } from "./components/ui/label";
 import { PARTNERHSIPS_UI_PATH } from "./lib/values";
-
+//
 const LoginPage = () => {
   const router = useRouter();
 
