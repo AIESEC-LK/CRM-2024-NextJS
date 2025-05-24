8ddc150120f88b2ef16d2217f0d843bf08c7a484
bhanusunrise <pasindubhanukagood@gmail.com>
2025-05-24 06:23:15 +0530

    Data fetching debug

--- src/app/dashboard/prospect/prospects/page.tsx
+++ src/app/dashboard/prospect/prospects/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
   const baseUrl = process.env.BASE_URL || 'https://localhost:3000/'; // adjust for prod
 
   try {
-    const prospect_response = await fetch(`${baseUrl}api_new/prospects/get_all_prospects/`);
+    const prospect_response = await fetch(`${baseUrl}api_new/prospects/get_all_prospects`);
     if (!prospect_response.ok) {
       throw new Error(`Failed to fetch prospect data: ${prospect_response.statusText}`);
     }
@@ -16,6 +16,6 @@ export default async function Page() {
       </>;
   } catch (error) {
     console.error("Error in fetching data:", error);
-    return <div>Error loading prospect data. Please try again later.</div>;
+    return <div>Error loading prospect data. {String(error)}</div>;
   }
 }
